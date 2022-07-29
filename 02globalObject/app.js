809129c7374bb2b66d116820161f8086dd85c027
unknown <ebasansean@gmail.com>
2022-07-28 18:29:52 -0700

    changes00

---
+++ 02globalObject/app.js
@@ -0,0 +1,15 @@
+
+console.log(); // global
+
+setTimeout(); 
+// call a function after a delay
+// standard JavaScript. You can use this on a client, on a browser, or in Node.
+clearTimeout();
+
+setInterval(); 
+// repeatedly call a function after an increment of delay.
+clearInterval();
+// to stop that function from calling repeatedly.
+
+// These are global objects. There are other that we're gonna learn later on.
+// 17:00 - continuation.
