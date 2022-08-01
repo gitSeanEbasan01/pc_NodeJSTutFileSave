e8bc7a094b7f1203e94bb231c3bed442781b9de0
unknown <ebasansean@gmail.com>
2022-07-31 19:43:50 -0700

    update00

--- 02globalObject/app.js
+++ 02globalObject/app.js
@@ -12,4 +12,16 @@ clearInterval();
 // to stop that function from calling repeatedly.
 
 // These are global objects. There are other that we're gonna learn later on.
-// 17:00 - continuation.
+
+
+// In browsers, we have this... windows object that represents our global scope.
+// So all the variables and functions that are defined globally, we can access them via the window object.
+window.setTimeout
+window.console.log // or simply..
+console.log
+
+var message = ''; // you can access them with...
+window.message
+
+// In node, we don't have this widow object. Instead we have global.
+// 18:00 - continuation
